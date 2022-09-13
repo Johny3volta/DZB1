76af715d35f73173d2c3de03b90f2dc5b3623a62
3v <jarovcev@mail.ru>
2022-09-13 21:31:07 +0300

    3

--- 1Task/Program.cs
+++ 1Task/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.Write("Введите Первое число  ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Второе число  ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("A^B = " + Math.Pow(A, B));
