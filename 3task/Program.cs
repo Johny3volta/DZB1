88f106474e5f95fcaa24b041963715fe46f583b3
3v <jarovcev@mail.ru>
2022-09-13 21:18:15 +0300

    3

--- 3task/Program.cs
+++ 3task/Program.cs
@@ -3,7 +3,7 @@
 int[] array = new int[8];
 
 for (int i = 0; i < array.Length; i++)
-array[i] = new Random().Next(0, 99);
+array[i] = new Random().Next(0, 100);
 
 Console.WriteLine("[" + string.Join(", ", array) + "]");
 
